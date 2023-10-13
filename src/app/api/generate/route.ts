795b205995f1278518b1e1ffebfebae116814ee9
adil <68734155+adevinwild@users.noreply.github.com>
2023-10-13 09:07:53 +0200

    feat: :sparkles: Added webhook and predictions
    - Added appwrite to manage webhook - Added a new model version - Updated UI and UI logic

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,15 +1,15 @@
+import { Ratelimit } from "@upstash/ratelimit";
 import { put } from "@vercel/blob";
 import { kv } from "@vercel/kv";
-import { Ratelimit } from "@upstash/ratelimit";
-
 import { nanoid } from "nanoid";
 import { NextRequest, NextResponse } from "next/server";
+import Appwrite from "~/server/appwrite";
 
 import { MAX_FILE_SIZE } from "~/lib/constants";
 import replicate from "~/server/replicate";
 
 const ratelimit = new Ratelimit({
-  limiter: Ratelimit.slidingWindow(15, "1 d"), // 10 requests per 1 day
+  limiter: Ratelimit.slidingWindow(150, "1 d"), // 10 requests per 1 day
   redis: kv,
 });
 
@@ -57,18 +57,52 @@ export async function POST(req: NextRequest) {
     access: "public",
   });
 
-  const output = await replicate.run(
-    "cjwbw/real-esrgan:d0ee3d708c9b911f122a4ad90046c5d26a0293b99476d697f6bb7f2e251ce2d4",
-    {
-      input: {
-        image: blob.url,
-        upscale: 4,
-      },
-    }
-  );
+  const SITE_URL =
+    process.env.NODE_ENV === "development"
+      ? "https://7f8f-2a01-cb1c-8104-d500-a98d-c719-3dd3-d02c.ngrok-free.app"
+      : "https://micro-scale.vercel.app";
 
-  return NextResponse.json({
-    improved: output,
-    original: blob.url,
-  });
+  try {
+    const savedPrediction = await Appwrite.database.createDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_APPWRITE_PREDICTIONS_ID as string,
+      "", // unique-id if empty
+      {
+        is_generated: false,
+        output: null,
+        input: blob.url,
+      }
+    );
+
+    const webhook = new URL(`${SITE_URL}/api/webhook/save-image`);
+    // setting an id to the webhook so we can identify the prediction later
+    webhook.searchParams.set("id", savedPrediction.$id);
+
+    const prediction = await replicate.predictions.create({
+      version:
+        "42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b",
+      input: {
+        scale: 4,
+        image: blob.url,
+      },
+      webhook: webhook.toString(),
+      webhook_events_filter: ["completed"],
+    });
+
+    if (prediction.error) {
+      return NextResponse.json(
+        { message: "Something went wrong" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({
+      id: savedPrediction.$id,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
