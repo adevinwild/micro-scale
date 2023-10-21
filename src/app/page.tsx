3edb38135b1abc6c8ffbfd99daedb5d241db94d6
adil <68734155+adevinwild@users.noreply.github.com>
2023-10-21 21:59:28 +0200

    feat: :sparkles: Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,15 @@ const isMaintenance = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
 export default function Page() {
   return (
     <main className="grid content-start gap-y-4 justify-center min-h-[100svh] items-center p-6 lg:p-16 h-full bg-zinc-100 dark:bg-zinc-950 smooth">
-      <NavigationMenu />
-      {isMaintenance ? <MaintenanceMode /> : <Generate />}
-      <Footer />
+      {isMaintenance ? (
+        <MaintenanceMode />
+      ) : (
+        <>
+          <NavigationMenu />
+          <Generate />
+          <Footer />
+        </>
+      )}
     </main>
   );
 }
